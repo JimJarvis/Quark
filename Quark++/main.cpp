d5859b8b5e71d3c1d9b105834aff04bfc77dcc34
JimJarvis <jimfanspire@gmail.com>
2014-08-03 10:17:04 -0400

    Add Eigen library support

--- Quark++/main.cpp
+++ Quark++/main.cpp
@@ -1,6 +1,8 @@
 #include "qureg.h"
+#include <Eigen/Dense>
 #include "vld.h"
 using namespace Testing;
+using Eigen::Matrix2cf;
 
 int main(int argc, char **argv)
 {
@@ -9,6 +11,14 @@ int main(int argc, char **argv)
 	qureg = Qureg(3, 5);
 
 	pr(qureg);
+
+	Matrix2cf m;
+	m(0, 0) = CX(2,3);
+	m(0, 1) = CX(3,4);
+	m(1, 0) = CX(1,1);
+	m(1, 1) = CX(2,3);
+	pr(m);
+	pr(m.adjoint());
 
 	return 0;
 }
