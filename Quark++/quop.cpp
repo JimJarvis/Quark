ceb6c0190ed63a8f7e3f5047d5b79ead4c6117f8
JimJarvis <jimfanspire@gmail.com>
2014-08-03 14:43:55 -0400

    NEW quop.cpp/.h for quantum gates and operations

---
+++ Quark++/quop.cpp
@@ -0,0 +1,7 @@
+/**********************************************
+* Quantum operations and gates  *
+**********************************************/
+#include "quop.h"
+#include "qureg.h"
+
+using namespace Quop;
---
+++ Quark++/quop.h
@@ -0,0 +1,10 @@
+#ifndef quop_h__
+#define quop_h__
+
+#include "qureg.h"
+namespace Quop // quantum operations
+{
+
+}
+
+#endif // quop_h__
