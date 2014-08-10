0a1ff423c07dbfbb5f0c40067e6186bd014e87cb
JimJarvis <jimfanspire@gmail.com>
2014-08-10 01:12:58 -0400

    Make ASSERT_CX_EQ a smart macro

--- Quark++/tests/tests.h
+++ Quark++/tests/tests.h
@@ -13,12 +13,9 @@ using namespace Eigen;
 ///////************** Test conventions **************///////
 #define QubitRange(start) Range<>(start, 8)
 #define QubaseRange(nqubit) Range<qubase>(1 << nqubit)
-
-INLINE void ASSERT_CX_EQ(const CX& cx1, const CX& cx2, const string& errstr = "", float tol = TOL)
-{
-	ASSERT_NEAR(cx1.real(), cx2.real(), tol) << errstr;
-	ASSERT_NEAR(cx1.imag(), cx2.imag(), tol) << errstr;
-}
+#define ASSERT_CX_EQ(cx1, cx2, errpipe) \
+	ASSERT_NEAR(cx1.real(), cx2.real(), TOL) << errpipe; \
+	ASSERT_NEAR(cx1.imag(), cx2.imag(), TOL) << errpipe;
 
 /*
 *	Error stream must be terminated by "Eend"
@@ -30,21 +27,14 @@ inline void ASSERT_MAT(const MatrixXcf& m1, const MatrixXcf& m2, const string& e
 	ASSERT_EQ(r = m1.rows(), m2.rows()) << "Row dims should agree";
 	ASSERT_EQ(c = m1.cols(), m2.cols()) << "Col dims should agree";
 
-	auto genError = [&](size_t i, size_t j)
-	{
-		ostringstream oss;
-		oss << "Disagree at [" << i << ", " << j << "]: "
-			<< m1(i, j) << " vs " << m2(i, j) << endl;
-		return oss.str() + errstr;
-	};
-
+	CX a1, a2;
 	for (size_t i = 0; i < r; ++i)
 	for (size_t j = 0; j < c; ++j)
 	{
-		CX a1 = m1(i, j);
-		CX a2 = m2(i, j);
-		ASSERT_NEAR(a1.real(), a2.real(), tol) << genError(i, j);
-		ASSERT_NEAR(a1.imag(), a2.imag(), tol) << genError(i, j);
+		a1 = m1(i, j); a2 = m2(i, j);
+		ASSERT_CX_EQ(a1, a2, 
+					 "Disagree at [" << i << ", " << j << "]: " 
+					 << m1(i, j) << " vs " << m2(i, j) << endl << errstr);
 	}
 }
 
