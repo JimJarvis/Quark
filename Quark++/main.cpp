9f5d7c2626d5471cad6c59797813bdd513178474
JimJarvis <jimfanspire@gmail.com>
2014-09-20 02:07:34 -0400

    Add prompt loop

--- Quark++/main.cpp
+++ Quark++/main.cpp
@@ -9,13 +9,14 @@ int main(int argc, char **argv)
 {
 	ptitle("SHOR's ALGORITHM SIMULATION");
 	pr("");
-	bool mode = true;
-	if (argc == 3)
-		mode = str2int(argv[2]);
+	while (1)
+	{
+		int M;
+		cout << "\nEnter an integer to factorize: ";
+		cin >> M;
 
-	int M = str2int(argv[1]);
-	auto ans = shor_factorize(std::ceil(log2(M)), M, mode);
-	pr("Factorize " << M << " = " << ans.first << " * " << ans.second);
-
+		auto ans = shor_factorize(std::ceil(log2(M)), M, true);
+		pr("\nFactorize " << M << " = " << ans.first << " * " << ans.second);
+	}
 	return 0;
 }
